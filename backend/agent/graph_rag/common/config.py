43e1e4c31d7610f20ded9c52dd799ea0a701ff52
Ducp <126154839+ducpter@users.noreply.github.com>
2025-08-28 00:37:06 +0700

    kill conflict

--- backend/agent/graph_rag/common/config.py
+++ backend/agent/graph_rag/common/config.py
@@ -7,21 +7,17 @@ load_dotenv()
 NEO4J_URI = os.getenv("NEO4J_URI", "neo4j://localhost:7687")
 NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "12345678")
-NEO4J_DATABASE = os.getenv("NEO4J_DATABASE", "testing")
+NEO4J_DATABASE = os.getenv("NEO4J_DATABASE", "neo4j")
 
 # File paths
 DOCLING_INPUT_PATH = "backend/uploaded_files/Quy trinh Kiem soat chi va Thanh toan cua UET (03.01.2021).docx"
 <<<<<<< HEAD
 DOCLING_OUTPUT_PATH = "backend/json_output/chapter_3.json"
 TEXT_JSON_PATH = "json_text/quytrinh.json"
-=======
-DOCLING_OUTPUT_PATH = "backend/json_output/chapter_5.json"
-TEXT_JSON_PATH = "backend/output_quytrinh.json"
->>>>>>> aec220d7600dfd678cef21978428c414c9caf1e2
 
 # LLM configurations
 GEMINI_MODEL = os.getenv("GEMINI_MODEL", "gemini-2.0-flash")
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyBs2v-H_iY97_xWOn2F0jwCNEyxOulOLrE")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyCs1XRT1wkGIXGj-1XvV4nYCCpQj4PfhIk")
 
 # Cypher queries
 WIPE_ALL_CYPHER = "MATCH (n) DETACH DELETE n"
